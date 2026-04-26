82a05593a122cdda3777f4d327d9befa4054bd5c
Chiranjeevi Ramamurthy <chiranram7@live.com>
2026-04-26 13:10:20 +0530

    Initial Commit

---
+++ Locolarm/LocolarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocolarmApp.swift
+//  Locolarm
+//
+//  Created by Chiranjeevi Ram on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct LocolarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
